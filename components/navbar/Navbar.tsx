88e16aa5cdfb74b29208f9f64181bd80fa889bc4
genia <wayra204genia@gmail.com>
2024-06-03 19:05:15 -0400

    Link-Dinamicos

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,3 +1,12 @@
+import { HomeIcon } from "@primer/octicons-react"
+import Link from "next/link"
+
+const navItems =[
+  {path: '/about', text: 'About'},
+  {path: '/pricing', text: 'Pricing'},
+  {path: '/contact', text: 'Contact'},
+]
+
 
 
 export const Navbar = () => {
@@ -5,12 +14,23 @@ export const Navbar = () => {
 
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link href={'/'} className="flex items-center">
+        <HomeIcon className="mr-2"/>
+        <span>Home</span>
+        </Link>
+      
       <div className="flex flex-1"></div>
 
-      <a className="mr-2" href="/about">About</a>
-      <a className="mr-2" href="/pricing">Pricing</a>
-      <a className="mr-2" href="/contact">Contact</a>
+      {/* crear de forma dinamica */}
+      {
+        navItems.map(navItem =>(
+          <Link key={navItem.path} className="mr-2" href={navItem.path}>{navItem.text}</Link>
+        ))
+      }
+
+     {/*  <Link className="mr-2" href="/about">About</Link>
+      <Link className="mr-2" href="/pricing">Pricing</Link>
+      <Link className="mr-2" href="/contact">Contact</Link> */}
     </nav>
   )
 }
